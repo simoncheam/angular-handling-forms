42553f4dbbe55d86aa7f8c295126ea4777d9ace5
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-24 11:45:49 -0400

    completed template driven form demo walkthrough, all functions working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
+  defaultQuestion = "pet"; // ! Add default question for ngModel binding
+  answer = "";
+  genders = ["male", "female"]; //! for radio buttons
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
+
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+    // this.signupForm.setValue({ //! this is not the best approach
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+
+    // ! how we can overwrite specific fields
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(this.signupForm);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret; // !needs to match "name on input control"
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
